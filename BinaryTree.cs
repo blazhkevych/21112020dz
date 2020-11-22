23ea1b942e836ee54247edd548c0c29a3054f2af
Viktor Blazhkevych <blazhkevych.va@gmail.com>
2020-11-22 18:21:04 +0200

    Add project files.

---
+++ BinaryTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _21112020dz
+{
+    public class BinaryTree<T> : IEnumerable<T> where T : IComparable
+    {
+        public BinaryTree<T> Root { get; private set; }
+        public BinaryTree<T> Left { get; private set; }
+        public BinaryTree<T> Right { get; private set; }
+        public T Value { get; private set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GoThroughTree().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T value)
+        {
+            if (Root == null)
+            {
+                Value = value;
+                Root = this;
+                return;
+            }
+
+            var current = Root;
+            while (current != null)
+                if (value.CompareTo(current.Value) > 0)
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = new BinaryTree<T> { Value = value };
+                        return;
+                    }
+
+                    current = current.Right;
+                }
+                else
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = new BinaryTree<T> { Value = value };
+                        return;
+                    }
+
+                    current = current.Left;
+                }
+        }
+
+        private IEnumerable<T> GoThroughTree()
+        {
+            if (Root == null) yield break;
+
+            var stack = new Stack<BinaryTree<T>>();
+            var current = Root;
+
+            while (stack.Count > 0 || current != null)
+                if (current == null)
+                {
+                    current = stack.Pop();
+                    yield return current.Value;
+                    current = current.Right;
+                }
+                else
+                {
+                    stack.Push(current);
+                    current = current.Left;
+                }
+        }
+    }
+
+    public class BinaryTree
+    {
+        public static BinaryTree<T> Create<T>(params T[] arr) where T : IComparable
+        {
+            var res = new BinaryTree<T>();
+            foreach (var el in arr)
+                res.Add(el);
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//ЗАДАНИЕ «Бинарное дерево»
+//В проекте Generics.BinaryTrees создайте  класс бинарного дерева поиска так, чтобы он проходил приложенные тесты. 
+//Оптимизируйте код метода GetEnumerable так, чтобы он работал за O(n) по времени, где n - количество элементов в дереве, и O(1) по памяти.
+
+using NUnitLite;
+
+namespace _21112020dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new AutoRun().Execute(args);
+        }
+    }
+}
